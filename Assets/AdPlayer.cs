cbf49c665294e5e6848def516ee2f9aa543ce221
Tony Godfrey <tonygod@sharkappsllc.com>
2017-02-13 20:53:34 -0800

    Adding original files

---
+++ Assets/AdPlayer.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdPlayer : MonoBehaviour
+{
+	bool hasPlayed = false;
+
+	private void Start()
+	{
+		//StartCoroutine(AdPlay(1));
+		StartCoroutine(RewardedAdPlay(5));
+	}
+
+	IEnumerator AdPlay(float seconds)
+	{
+		while (!hasPlayed)
+		{
+			ShowAd();
+			yield return new WaitForSeconds(seconds);
+		}
+	}
+
+
+	IEnumerator RewardedAdPlay(float seconds)
+	{
+		while (!hasPlayed)
+		{
+			ShowRewardedAd();
+			yield return new WaitForSeconds(seconds);
+		}
+	}
+
+	public void ShowAd()
+	{
+		if (Advertisement.IsReady())
+		{
+			hasPlayed = true;
+			Advertisement.Show();
+		}
+	}
+
+	public void ShowRewardedAd()
+	{
+		if (Advertisement.IsReady("rewardedVideoZone"))
+		{
+			hasPlayed = true;
+			Debug.Log("Showing Rewarded Ad");
+			var options = new ShowOptions { resultCallback = HandleShowResult };
+			Advertisement.Show("rewardedVideoZone", options);
+		} else
+		{
+			Debug.Log("Rewareded Ad not ready");
+		}
+	}
+
+	private void HandleShowResult(ShowResult result)
+	{
+		switch (result)
+		{
+			case ShowResult.Finished:
+				Debug.Log("The ad was successfully shown.");
+				hasPlayed = true;
+				//
+				// YOUR CODE TO REWARD THE GAMER
+				// Give coins etc.
+				break;
+			case ShowResult.Skipped:
+				Debug.Log("The ad was skipped before reaching the end.");
+				break;
+			case ShowResult.Failed:
+				Debug.LogError("The ad failed to be shown.");
+				break;
+		}
+	}
+}
---
+++ Assets/AnalyticsModule.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AnalyticsModule : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SetCountry();
+		BuyLevelIncrease();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void BuyLevelIncrease()
+	{
+		Analytics.Transaction("LevelIncrease", 1, "EUR");
+		LevelUp();
+	}
+
+	void LevelUp()
+	{
+		Analytics.CustomEvent("levelUp", new Dictionary<string, object>
+		{
+			{ "newLevel", 2 },
+			{ "trigger", "purchase" },
+		});
+	}
+
+	void SetCountry()
+	{
+		Analytics.CustomEvent("demographics", new Dictionary<string, object>
+		{
+			{ "country", "UK" },
+		});
+	}
+}
